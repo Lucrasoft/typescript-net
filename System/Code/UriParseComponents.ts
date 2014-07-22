5a4b6fa0eaec8af998b8915b652569d25382fdc8
Chris Vos <Chris@lucrasoft.nl>
2014-07-22 17:27:42 +0200

    Added all files from Mono/mcs/sys/sys. Mostly commented

---
+++ System/Code/UriParseComponents.ts
@@ -0,0 +1,259 @@
+﻿//todo
+
+//namespace System {
+	
+//	internal class ParserState
+//	{
+//    public ParserState(string uri)
+//		{
+//        remaining = uri;
+//        elements = new UriElements();
+//		}
+
+//		public string remaining;
+//		public UriElements elements;
+//	}
+
+//	// Parse Uri components (scheme, userinfo, host, query, fragment)
+//	// http://www.ietf.org/rfc/rfc3986.txt
+//	internal static class UriParseComponents
+//	{
+//		public static UriElements ParseComponents(string uri)
+//		{
+//			ParserState state = new ParserState(uri);
+			
+//			bool ok = ParseScheme(ref state);
+//    if (ok)
+//        ok = ParseAuthority(ref state);
+//    if (ok)
+//        ok = ParsePath(ref state);
+//    if (ok)
+//        ok = ParseQuery(ref state);
+//    if (ok)
+//        ParseFragment(ref state);
+
+//    return state.elements;
+//		}
+//				// ALPHA
+//		private static bool IsAlpha(char ch)
+//		{
+//    return (('a' <= ch) && (ch <= 'z')) ||
+//        (('A' <= ch) && (ch <= 'Z'));
+//		}
+
+//		// 3.1) scheme      = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
+//		private static bool ParseScheme(ref ParserState state) 
+//		{
+//			string part = state.remaining;
+
+//    if (!IsAlpha(part[0]))
+//        return part.Length > 0;
+			
+//			StringBuilder sb = new StringBuilder();
+//    sb.Append(part[0]);
+			
+//			int index;
+//    for (index = 1; index < part.Length; index++) {
+//				char ch = part[index];
+//        if (ch != '.' && ch != '-' && ch != '+' && !IsAlpha(ch))
+//            break;
+
+//        sb.Append(ch);
+//    }
+
+//    if (index + 1 <= part.Length && part[index] == ':') {
+//        state.elements.scheme = sb.ToString();
+//        state.remaining = part.Substring(index + 1);
+//    }
+
+//    return state.remaining.Length > 0;
+//		}
+
+//		private static bool ParseAuthority(ref ParserState state)
+//		{
+//			string part = state.remaining;
+
+//    if (part.Length < 2 || part[0] != '/' || part[1] != '/')
+//        return part.Length > 0;
+
+//    state.remaining = part.Substring(2);
+			
+//			bool ok = ParseUser(ref state);
+//    if (ok)
+//        ok = ParseHost(ref state);
+//    if (ok)
+//        ok = ParsePort(ref state);
+//    return ok;
+//		}
+
+//		static bool IsUnreserved(char ch)
+//		{
+//    return ch == '-' || ch == '.' || ch == '_' || ch == '~';
+//		}
+
+
+//		static bool IsSubDelim(char ch)
+//		{
+//    return ch == '!' || ch == '$' || ch == '&' || ch == '\'' || ch == '(' || ch == ')' ||
+//        ch == '*' || ch == '+' || ch == ',' || ch == ';' || ch == '=';
+//		}
+
+//		// userinfo    = *( unreserved / pct-encoded / sub-delims / ":" )
+//		private static bool ParseUser(ref ParserState state)
+//		{
+//			string part = state.remaining;
+//			StringBuilder sb = null;
+
+//			int index;
+//    for (index = 0; index < part.Length; index++) {
+//				char ch = part[index];
+
+//        if (ch == '%') {
+//            if (!Uri.IsHexEncoding(part, index))
+//                return false;
+//            ch = Uri.HexUnescape(part, ref index);
+//        }
+
+//        if (Char.IsLetterOrDigit(ch) || IsUnreserved(ch) || IsSubDelim(ch) || ch == ':') {
+//            if (sb == null)
+//                sb = new StringBuilder();
+//            sb.Append(ch);
+//        } else
+//            break;
+//    }
+
+//    if (index + 1 <= part.Length && part[index] == '@') {
+//        state.elements.user = sb == null ? "" : sb.ToString();
+//        state.remaining = state.remaining.Substring(index + 1);
+//    }
+
+//    return state.remaining.Length > 0;
+//		}
+
+//		// host        = IP-literal / IPv4address / reg-name
+//		private static bool ParseHost(ref ParserState state)
+//		{
+//			string part = state.remaining;
+//			StringBuilder sb = new StringBuilder();
+			
+//			int index;
+//    for (index = 0; index < part.Length; index++) {	
+				
+//				char ch = part[index];
+
+//        if (ch == '/' || ch == ':' || ch == '#' || ch == '?')
+//            break;
+
+//        sb.Append(ch);
+//    }
+
+//    if (index <= part.Length)
+//        state.remaining = part.Substring(index);
+
+//    state.elements.host = sb.ToString();
+
+//    return state.remaining.Length > 0;
+//		}
+
+//		// port          = *DIGIT
+//		private static bool ParsePort(ref ParserState state)
+//		{
+//			string part = state.remaining;
+//    if (part.Length == 0 || part[0] != ':')
+//        return part.Length > 0;
+			
+//			StringBuilder sb = new StringBuilder();
+			
+//			int index;
+//    for (index = 1; index < part.Length; index++) {
+//				char ch = part[index];
+
+//        if (!char.IsDigit(ch))
+//            break;
+
+//        sb.Append(ch);
+//    }
+
+//    if (index <= part.Length)
+//        state.remaining = part.Substring(index);
+
+//    state.elements.port = sb.ToString();
+
+//    return state.remaining.Length > 0;
+//		}
+
+//		private static bool ParsePath(ref ParserState state)
+//		{
+//			string part = state.remaining;
+//			StringBuilder sb = new StringBuilder();
+			
+//			int index;
+//    for (index = 0; index < part.Length; index++) {
+				
+//				char ch = part[index];
+
+//        if (ch == '#' || ch == '?')
+//            break;
+
+//        sb.Append(ch);
+//    }
+
+//    if (index <= part.Length)
+//        state.remaining = part.Substring(index);
+
+//    state.elements.path = sb.ToString();
+
+//    return state.remaining.Length > 0;
+//		}
+
+//		private static bool ParseQuery(ref ParserState state)
+//		{
+//			string part = state.remaining;
+
+//    if (part.Length == 0 || part[0] != '?')
+//        return part.Length > 0;
+			
+//			StringBuilder sb = new StringBuilder();
+			
+//			int index;
+//    for (index = 1; index < part.Length; index++) {
+				
+//				char ch = part[index];
+
+//        if (ch == '#')
+//            break;
+
+//        sb.Append(ch);
+//    }
+
+//    if (index <= part.Length)
+//        state.remaining = part.Substring(index);
+
+//    state.elements.query = sb.ToString();
+
+//    return state.remaining.Length > 0;
+//		}
+
+//		private static bool ParseFragment(ref ParserState state)
+//		{
+//			string part = state.remaining;
+
+//    if (part.Length == 0 || part[0] != '#')
+//        return part.Length > 0;
+			
+//			StringBuilder sb = new StringBuilder();
+			
+//			int index;
+//    for (index = 1; index < part.Length; index++) {	
+				
+//				char ch = part[index];
+
+//        sb.Append(ch);
+//    }
+
+//    state.elements.fragment = sb.ToString();
+
+//    return false;
+//		}
+//	}
+//}
---
+++ System/Code/UriTypeConverter.ts
@@ -0,0 +1,115 @@
+﻿//todo
+
+module System {
+
+	export class UriTypeConverter /* extends TypeConverter */{
+
+//		public UriTypeConverter()
+//		{
+//		}
+
+
+//		private bool CanConvert(Type type)
+//		{
+//        if (type == typeof (string))
+//            return true;
+//        if (type == typeof (Uri))
+//				return true;
+//#if NET_2_1
+//			return false;
+//#else
+//			return (type == typeof (InstanceDescriptor));
+//#endif
+//		}
+
+//		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+//		{
+//        if (sourceType == null)
+//            throw new ArgumentNullException("sourceType");
+
+//        return CanConvert(sourceType);
+//		}
+
+//		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+//		{
+//        if (destinationType == null)
+//            return false;
+
+//        return CanConvert(destinationType);
+//		}
+
+//		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+//		{
+//        if (value == null) {
+//#if NET_2_1
+//				throw new NotSupportedException(Locale.GetText("Cannot convert from value."));
+//#else
+//				throw new ArgumentNullException("value");
+//#endif
+//			}
+
+//        if (!CanConvertFrom(context, value.GetType()))
+//            throw new NotSupportedException(Locale.GetText("Cannot convert from value."));
+
+//			if (value is Uri)
+//				return value;
+
+//			string s = (value as string);
+//        if (s != null) {
+//#if NET_2_1
+//				if (s == "")
+//					return null;
+//#endif
+//				return new Uri(s, UriKind.RelativeOrAbsolute);
+//			}
+
+//#if !NET_2_1
+//			InstanceDescriptor id = (value as InstanceDescriptor);
+//        if (id != null) {
+//            return id.Invoke();
+//			}
+//#endif
+//			return base.ConvertFrom(context, culture, value);
+//		}
+
+//		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+//		{
+//        if (!CanConvertTo(context, destinationType))
+//            throw new NotSupportedException(Locale.GetText("Cannot convert to destination type."));
+
+//			Uri uri = (value as Uri);
+//        if (uri != null) {
+//            if (destinationType == typeof (string))
+//                return uri.ToString();
+//            if (destinationType == typeof (Uri))
+//					return uri;
+//#if !NET_2_1
+//				if (destinationType == typeof (InstanceDescriptor)) {
+//					ConstructorInfo ci = typeof (Uri).GetConstructor(new Type[2] { typeof (string), typeof (UriKind) });
+//                    return new InstanceDescriptor(ci, new object []{ uri.ToString(), uri.IsAbsoluteUri ? UriKind.Absolute : UriKind.Relative });
+//				}
+//#else
+//				throw new NotSupportedException(Locale.GetText("Cannot convert to destination type."));
+//#endif
+//			}
+
+//#if NET_2_1
+//			throw new NotSupportedException(Locale.GetText("Cannot convert to destination type."));
+//#else
+//			return base.ConvertTo(context, culture, value, destinationType);
+//#endif
+//		}
+
+//#if !NET_2_1
+//		public override bool IsValid(ITypeDescriptorContext context, object value)
+//		{
+//        if (value == null)
+//            return false;
+
+//			// LAMESPEC: docs says that a string is valid only if we can create an URI
+//			// from it. However all strings seems to be accepted (see unit tests)
+//			return ((value is string) || (value is Uri));
+//		}
+//#endif
+	}
+}
